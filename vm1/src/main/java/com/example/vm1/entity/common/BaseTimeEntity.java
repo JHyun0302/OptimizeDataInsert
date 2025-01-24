1dee84c62df2a57869fe48c6f3d205c1b3949b34
JHyun <wogustkfkd11@naver.com>
2025-01-24 14:01:38 +0900

    Ver3. DB Partitioning

--- vm1/src/main/java/com/example/vm1/entity/common/BaseTimeEntity.java
+++ vm1/src/main/java/com/example/vm1/entity/common/BaseTimeEntity.java
@@ -1,5 +1,6 @@
 package com.example.vm1.entity.common;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
@@ -15,9 +16,11 @@ import java.time.LocalDateTime;
 public class BaseTimeEntity {
 
     @CreatedDate
+    @Column(name = "CREATED_DATE", nullable = false, updatable = false)
     private LocalDateTime createdDate;
 
     @LastModifiedDate
+    @Column(name = "MODIFIED_DATE", nullable = false)
     private LocalDateTime modifiedDate;
 
 }
