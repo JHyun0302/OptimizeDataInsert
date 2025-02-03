4ff46955ea8bb842fe38fde2e578289f3f818ee6
JHyun <wogustkfkd11@naver.com>
2025-02-03 10:10:47 +0900

    Ver4. 테스트 완료

--- vm3/src/main/java/com/example/vm3/scheduler/DummyDataScheduler.java
+++ vm3/src/main/java/com/example/vm3/scheduler/DummyDataScheduler.java
@@ -3,6 +3,7 @@ package com.example.vm3.scheduler;
 import com.example.vm3.redis.RedisStreamConsumer;
 import com.example.vm3.service.DummyDataInsertService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -14,9 +15,15 @@ public class DummyDataScheduler {
 
     private final DummyDataInsertService dummyDataInsertService;
 
-    @Scheduled(fixedRate = 10000) // 10초마다 실행
-    public void scheduleInsertDummyData() {
-        redisStreamConsumer.consumeStream();
-        dummyDataInsertService.insertDummyData();
+    @Value("${spring.application.role}")
+    private String role;
+
+    @Scheduled(fixedRate = 10000)
+    public void scheduleTask() {
+        if ("producer".equals(role)) {
+            dummyDataInsertService.insertDummyData(); // 데이터 생성
+        } else if ("consumer".equals(role)) {
+            redisStreamConsumer.consumeStream(); // 데이터 소비
+        }
     }
 }
